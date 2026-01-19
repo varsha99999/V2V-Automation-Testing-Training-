1ea84535fa329eae4a83515829420e1ca9a6b7ea
varsha <130341958+varsha99999@users.noreply.github.com>
2026-01-19 22:49:05 +0530

    Add files via upload

---
+++ pratice.js
@@ -0,0 +1,160 @@
+/*sign ofproduct of three num*/
+/*let a=3
+let b=7
+let c=2
+if (a>=0 && b>=0 && c>=0){
+    console.log("the sign is +")
+}
+else{
+    console.log("the sign is -")
+
+}
+
+
+
+/* sorting the numbers
+ let a=0;
+ let b=-1;
+ let c=-5;
+ if (a>=b && a>=c){
+    if(b>=c){
+    console.log(a,b,c);
+    }
+    else{
+        console.log(a,c,b);
+    }
+    
+ }
+ else if(b>=a && b>=c){
+    if(a>=c){
+        console.log(b,a,c);
+    }
+    else{
+        console.log(b,c,a);
+    }
+ }
+ else if(c>=a && c>=b){
+    if(a>=b){
+        console.log(c,a,b);
+    }
+    else{
+        console.log(c,b,c);
+    }
+ }*/
+
+
+
+
+/*student grades calulation
+
+let david=80;
+let vinoth=77;
+let divya=88;
+let ishitha=95;
+let thomas=68;
+let score=((80+77+88+95+69)/5);
+console.log(score);
+if (score>90){
+    console.log("A")
+}
+else if(score>80){
+        console.log("B")
+    }
+
+
+else if(score>70){
+        console.log("C")
+    }
+
+else if(score>60){
+        console.log("D")
+    }
+
+else if(score>50){
+        console.log("F")
+    }*/
+
+
+/*  FIZZBUSS
+
+let num=5;
+if(num%3==0 && num%5==0){
+    console.log("FIZZBUZZ");
+}
+else if(num%5==0){
+    console.log("BUZZ");
+}
+else if(num%3==0){
+    console.log("FIZZ")
+}
+else{
+    console.log("not FIZZBUZZ")
+}*/
+
+
+
+// let day="thursday";
+// switch(day){
+//     case 'monday':
+//     case 'tuesday':
+//     case 'wednesday':
+//         console.log("8AM");
+//     break;
+//     case 'thursday':
+//     case 'friday':
+//         console.log("9AM");
+//     break;
+//     case 'saturday':
+//         console.log("10AM")
+//     break;
+// }
+
+/*let alien={
+    name:"varsha",
+    tech:"js",
+    laptop:{
+        cpu:17,
+        ram:4,
+        brand:"hp"
+    }
+}
+for (let key in alien.laptop){
+    console.log(key,alien.laptop[key]);
+}*/
+
+
+
+ /*let a=[1,2,3,4]
+a.push("varsha") 
+console.log(a);*/
+
+/*let num=[1,2,3,4,5];
+let [a,b,c,d,e]=num;
+[a,b,c,d,e]=[e,d,c,b,a]
+console.log(a,b,c,d,e);*/
+
+
+
+/*let array=[]
+if (array=([])){
+    console.log("its array")
+
+}
+else{
+    console.log("its not array")
+}
+*/
+
+
+let day="tuesday";
+switch(day){
+    case 'tuesday','monday','wednesday':
+        console.log("8AM");
+    break;
+    case 'Thursday','friday':
+        console.log("9AM");
+    break;
+    case 'saturday':
+        console.log("10AM")
+    break;
+}
